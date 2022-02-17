a2c798629045ca51469923879505b2a3c819106c
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-02-17 17:00:43 +0900

    feat: SPA 구성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Main } from './pages';
+import { GlobalStyle, Background } from './components';
 
 function App() {
   return (
-    <div></div>
+    <RecoilRoot>
+      <GlobalStyle />
+      <Background />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
