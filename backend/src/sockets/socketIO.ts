a280afc98dc0279ea00f706aec164c8384b0ddfe
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-03-11 20:07:15 +0900

    feat: 백단 정답 데이터 관리 및 무작위 노래 index 프론트단 전달

--- backend/src/sockets/socketIO.ts
+++ backend/src/sockets/socketIO.ts
@@ -7,6 +7,33 @@ interface IUserSocket {
   };
 }
 
+const AnswerList = [
+  "헤네시스",
+  "엘리니아",
+  "커닝시티",
+  "리스항구",
+  "페리온",
+  "슬리피우드",
+  "개미굴",
+  "헤네시스시장",
+  "리엔",
+  "에레브",
+  "엘나스",
+  "오르비스",
+  "아쿠아리움",
+  "루디브리엄",
+  "아랫마을",
+  "지구방위본부",
+  "무릉",
+  "백초마을",
+  "마가티아",
+  "아리안트",
+  "리프레",
+  "시간의신전",
+  "에델슈타인",
+  "더시드로비",
+];
+let AnswerNum = 0;
 const UserObj: IUserSocket = {};
 
 const socketIO = (server: any) => {
@@ -24,9 +51,17 @@ const socketIO = (server: any) => {
       }
     );
 
-    socket.on('send answer', (sendData: {userName: string, answer:string}) => {
-      io.emit('receive answer', sendData);
-    })
+    socket.on(
+      "send answer",
+      (sendData: { userName: string; answer: string }) => {
+        io.emit("receive answer", sendData);
+        if (sendData.answer === AnswerList[AnswerNum]) {
+          const randNum = Math.floor(Math.random() * AnswerList.length);
+          AnswerNum = randNum;
+          io.emit("correct answer", { flag: true, idx: AnswerNum });
+        }
+      }
+    );
 
     socket.on("disconnect", () => {
       delete UserObj[socket.id];
