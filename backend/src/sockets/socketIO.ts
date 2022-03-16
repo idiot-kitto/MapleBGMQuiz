7383cdc6c5814f78769710064425c9a2dad7f64e
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-03-16 16:09:25 +0900

    feat: 복수 정답 처리 로직 구현

--- backend/src/sockets/socketIO.ts
+++ backend/src/sockets/socketIO.ts
@@ -8,69 +8,75 @@ interface IUserSocket {
 }
 
 const AnswerList = [
-  "헤네시스",
-  "엘리니아",
-  "커닝시티",
-  "리스항구",
-  "페리온",
-  "슬리피우드",
-  "개미굴",
-  "헤네시스시장",
-  "리엔",
-  "에레브",
-  "엘나스",
-  "오르비스",
-  "아쿠아리움",
-  "루디브리엄",
-  "아랫마을",
-  "지구방위본부",
-  "무릉",
-  "백초마을",
-  "마가티아",
-  "아리안트",
-  "리프레",
-  "시간의신전",
-  "에델슈타인",
-  "더시드로비",
-  "코크타운",
-  "행복한마을",
-  "소멸의여로",
-  "츄츄아일랜드",
-  "레헬른",
-  "아르카나",
-  "모라스",
-  "에스페라",
-  "판테온",
-  "세르니움광장",
-  "호텔아르크스",
-  "리스토니아",
-  "뾰족귀여우마을",
-  "청운골",
-  "세계가끝나는곳",
-  "셀라스",
-  "캐시샵",
-  "몬스터라이프",
-  "무릉도장",
-  "갓오브컨트롤",
-  "플래그",
-  "마이스터빌",
-  "골드비치",
-  "플로리나비치",
-  "루타비스",
-  "피에르",
-  "블러디퀸",
-  "수련의숲",
-  "악몽의시계탑",
-  "루시드1페",
-  "루시드2페",
+  ["헤네시스"],
+  ["엘리니아"],
+  ["커닝시티"],
+  ["리스항구"],
+  ["페리온"],
+  ["슬리피우드"],
+  ["개미굴"],
+  ["헤네시스시장"],
+  ["리엔"],
+  ["에레브"],
+  ["엘나스"],
+  ["오르비스"],
+  ["아쿠아리움"],
+  ["루디브리엄"],
+  ["아랫마을"],
+  ["지구방위본부", "지방본"],
+  ["무릉"],
+  ["백초마을"],
+  ["마가티아"],
+  ["아리안트"],
+  ["리프레"],
+  ["시간의신전"],
+  ["에델슈타인"],
+  ["더시드로비", "더시드대기실"],
+  ["코크타운"],
+  ["행복한마을"],
+  ["소멸의여로", "여로"],
+  ["츄츄아일랜드", "츄츄"],
+  ["레헬른"],
+  ["아르카나"],
+  ["모라스"],
+  ["에스페라"],
+  ["판테온"],
+  ["세르니움광장", "전르니움", "세르니움"],
+  ["호텔아르크스", "아르크스"],
+  ["리스토니아"],
+  ["뾰족귀여우마을"],
+  ["청운골", "청운"],
+  ["세계가끝나는곳", "세끝"],
+  ["셀라스"],
+  ["캐시샵"],
+  ["몬스터라이프", "몬라"],
+  ["무릉도장", "무릉대청"],
+  ["갓오브컨트롤", "갓오컨"],
+  ["플래그", "플래그레이스"],
+  ["마이스터빌", "마빌"],
+  ["골드비치"],
+  ["플로리나비치"],
+  ["루타비스"],
+  ["피에르", "피에르보스맵"],
+  ["블러디퀸", "블러디퀸보스맵", "블퀸", "블퀸보스맵"],
+  ["수련의숲"],
+  ["악몽의시계탑", "시계탑"],
+  ["루시드1페", "루시드1페이즈"],
+  ["루시드2페", "루시드2페이즈"],
 ];
 
-let RandomAnswerNumArray:number[] = [];
-for(let i=0 ; i<AnswerList.length ; ++i) RandomAnswerNumArray.push(i);
+let RandomAnswerNumArray: number[] = [];
+for (let i = 0; i < AnswerList.length; ++i) RandomAnswerNumArray.push(i);
 RandomAnswerNumArray.sort(() => Math.random() - 0.5);
 
-let AnswerNum:number = 0;
+let AnswerNum: number = 0;
 const UserObj: IUserSocket = {};
+
+function checkAns(userInput: string) {
+  if (AnswerList[RandomAnswerNumArray[AnswerNum]].includes(userInput))
+    return true;
+  return false;
+}
 
 const socketIO = (server: any) => {
   const io = new Server(server);
@@ -92,15 +98,18 @@ const socketIO = (server: any) => {
       "send answer",
       (sendData: { socketID: string; userName: string; answer: string }) => {
         io.emit("receive answer", sendData);
-        if (sendData.answer.split(' ').join('') === AnswerList[RandomAnswerNumArray[AnswerNum]]) {
+        if (checkAns(sendData.answer.split(" ").join(""))) {
           AnswerNum++;
-          if(AnswerNum == AnswerList.length) {
+          if (AnswerNum == AnswerList.length) {
             RandomAnswerNumArray.sort(() => Math.random() - 0.5);
             AnswerNum = 0;
           }
           UserObj[sendData.socketID].answerNum++;
           io.emit("get current users", UserObj);
-          io.emit("correct answer", { flag: true, idx: RandomAnswerNumArray[AnswerNum] });
+          io.emit("correct answer", {
+            flag: true,
+            idx: RandomAnswerNumArray[AnswerNum],
+          });
         }
       }
     );
