7a53374939f39045aed2973585d5f314fdda4177
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2022-03-17 20:56:57 +0900

    feat: hooks를 이용한 alert modal 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Main } from './pages';
 import { GlobalStyle, Background } from './components';
+import AlertModal from 'components/common/alertmodal';
 
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
       <Background />
+      <AlertModal />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- frontend/src/recoil/common/index.tsx
+++ frontend/src/recoil/common/index.tsx
@@ -1,6 +1,20 @@
 import { atom } from 'recoil';
 
 export const currentUserName = atom({
-    key: 'currentUserName',
-    default: ''
+  key: 'currentUserName',
+  default: ''
+});
+
+export const alertState = atom({
+  key: 'alertState',
+  default: {
+    modalState: false,
+    comment: '',
+    bgColor: ''
+  }
+});
+
+export const timeOutValueState = atom({
+  key: 'timeOutValueState',
+  default: 0
 });
